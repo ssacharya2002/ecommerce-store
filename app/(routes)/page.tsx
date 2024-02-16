ab198ce8806baedba3d59ba6568cb0bdf0075b09
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2024-02-17 00:32:24 +0530

    featured products completed

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,7 +7,7 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const product = await getProducts({isFeatured:true});
+  const products = await getProducts({isFeatured:true});
 
   const billboard = await getBillboard("7f93d3bb-2169-496e-85ce-273c3b6e108a");
 
@@ -15,9 +15,9 @@ const HomePage = async () => {
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      </div>
       <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured products"  items={[]} />
+        <ProductList title="Featured products"  items={products} />
+      </div>
       </div>
     </Container>
   );
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,4 +1,6 @@
 import { Product } from "@/types";
+import NoResult from "@/components/ui/no-result";
+import ProductCard from "./ui/product-card";
 
 interface ProductListProps {
   title: string;
@@ -9,7 +11,14 @@ const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
 
     return(<div>
         <h3 className="font-bold text-3xl">{title}</h3>
-        <hr />
+        {items.length ===0 && <NoResult />}
+
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          {items.map((item)=>(
+           <ProductCard key={item.id} data={item}/>
+          ))}
+        </div>
     </div>)
 };
 
