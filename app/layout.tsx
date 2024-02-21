5d8af3f5b5cdcd871a932eb81e388f430ef79233
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2024-02-21 20:14:13 +0530

    product preview modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+import ModalProvider from "@/providers/modal-provider";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -19,10 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,21 +6,33 @@ import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
-
+  const previewModal = usePreviewModal();
   const router = useRouter();
 
-  const handleClick = () =>{
-    router.push(`/product/${data?.id}`)
-  }
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    console.log(data);
+    
+    previewModal.onOpen(data)
+  };
 
   return (
-    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 gap-y-4  ">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 gap-y-4  "
+    >
       {/* images and actions  */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
@@ -33,7 +45,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
@@ -46,18 +58,13 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
 
       {/* Description */}
       <div>
-        <p className="font-semibold text-lg">
-            {data.name}
-        </p>
-        <p className="text-sm text-gray-500">
-            {data.category?.name}
-        </p>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
       </div>
       {/* price */}
       <div className="flex items-center justify-center">
         <Currency value={data?.price} />
       </div>
-
     </div>
   );
 };
