e9ce9c4bc7ff2e14d57520596c3e73b8958ae091
Shaktishankar Acharya <86561217+ssacharya2002@users.noreply.github.com>
2024-02-29 12:37:03 +0530

    billboardId

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,17 +7,17 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const products = await getProducts({isFeatured:true});
+  const products = await getProducts({ isFeatured: true });
 
-  const billboard = await getBillboard("7f93d3bb-2169-496e-85ce-273c3b6e108a");
+  const billboard = await getBillboard(process.env.NEXT_PUBLIC_BILLBOARD_ID!);
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured products"  items={products} />
-      </div>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured products" items={products} />
+        </div>
       </div>
     </Container>
   );
